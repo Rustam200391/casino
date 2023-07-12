258024116142e25d35f726010998f7e0f8c2cfb1
Dmitriy Malakhov <mitia2022@gmail.com>
2023-07-12 17:29:04 +0200

    games grid + recent games table placeholder

--- src/components/pages/home/games-grid.tsx
+++ src/components/pages/home/games-grid.tsx
@@ -1,7 +1,79 @@
+import { Button, buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const GamesGrid = () => {
-  return <div>GamesGrid</div>;
+  return (
+    <div className="mt-10">
+      <p className="text-3xl font-bold">Добро пожаловать в Syndicate</p>
+      <div className="grid grid-cols-1 grid-rows-2 gap-4 mt-10 md:grid-cols-2 xl:grid-cols-11">
+        <GameCard
+          title="Jackpot"
+          href="/games/jackpot"
+          background={<Image src="/img/jackpot.png" alt="background" fill className="object-cover" />}
+          className="xl:col-span-5 xl:row-span-2"
+        />
+        <GameCard
+          title="Mines"
+          href="/games/jackpot"
+          background={<Image src="/img/register.jpg" alt="background" fill className="object-cover" />}
+          className="xl:col-span-3"
+        />
+        <GameCard
+          title="Crash"
+          href="/games/jackpot"
+          background={<Image src="/img/register.jpg" alt="background" fill className="object-cover" />}
+          className="xl:col-span-3"
+        />
+        <GameCard
+          title="Jackpot Dice"
+          href="/games/jackpot"
+          background={<Image src="/img/register.jpg" alt="background" fill className="object-cover" />}
+          className="xl:col-span-3"
+        />
+        <GameCard
+          title="Dices"
+          href="/games/jackpot"
+          background={<Image src="/img/register.jpg" alt="background" fill className="object-cover" />}
+          className="xl:col-span-3"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default GamesGrid;
+
+const GameCard = ({
+  className,
+  background,
+  href,
+  title,
+}: {
+  className?: string;
+  background: React.ReactNode;
+  href: string;
+  title: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        'px-4 py-3 border relative overflow-hidden border-gray-500 transition hover:scale-[102%] hover:border-gray-400 min-h-[200px]  rounded-3xl',
+        className
+      )}
+    >
+      {background}
+      <div className="relative flex flex-col justify-between h-full">
+        <div>
+          <p className="text-2xl font-bold">{title}</p>
+          <p className="text-sm text-gray-400">12 000 ₽</p>
+        </div>
+        <Link href={href} className={cn(buttonVariants({ variant: 'outline', size: 'lg' }), 'w-full')}>
+          Играть
+        </Link>
+      </div>
+    </div>
+  );
+};
--- src/components/pages/home/recent-games-table.tsx
+++ src/components/pages/home/recent-games-table.tsx
@@ -1,7 +1,97 @@
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import React from 'react';
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import { DataTable } from '@/components/ui/data-table';
 
-const RecentGamesTable = () => {
-  return <div>RecentGamesTable</div>;
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  id: string;
+  amount: number;
+  status: 'pending' | 'processing' | 'success' | 'failed';
+  email: string;
 };
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'status',
+    header: 'Игра',
+  },
+  {
+    accessorKey: 'email',
+    header: 'Имя игрока',
+  },
+  {
+    accessorKey: 'amount',
+    header: 'Ставка',
+  },
+  {
+    accessorKey: 'amount',
+    header: 'Коэффициент',
+  },
+  {
+    accessorKey: 'amount',
+    header: 'Получил',
+  },
+];
+
+function getData(): Payment[] {
+  // Fetch data from your API here.
+  return [
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    // ...
+  ];
+}
+
+function RecentGamesTable() {
+  const data = getData();
+
+  return (
+    <div className="mt-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}
 
 export default RecentGamesTable;
