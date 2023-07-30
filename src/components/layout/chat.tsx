897312ddfeec33e6d6a62c50c275a89dfb7fea6a
Maksim Peshekhonov <m.peshekhonov@bi.zone>
2023-07-31 01:06:36 +0300

    balance dropdown and modal draft, account dropdown

--- src/components/layout/chat.tsx
+++ src/components/layout/chat.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Chat = () => {
-  return <div className="col-span-3 border hidden lg:flex">Chat</div>;
+  return (
+    <div className="h-fit col-span-2 border border-neutral-800 rounded-3xl p-4 lg:flex lg:flex-col">
+      <div className="flex flex-col rounded-3xl text-center">
+        <p>В банке</p>
+        <p className="text-2xl font-bold">100 800 000 ₽</p>
+        <p className="text-xs">Розыгрыш через: 20 ч : 02 мин</p>
+        <div>ChatSettings</div>
+        <div>ChatHistory</div>
+        <hr className="border-neutral-800" />
+        ChatInput
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { FlagIcon, SettingsIcon, VolumeIcon, BellIcon } from 'lucide-react';
 
 const Sidebar = () => {
-  return <div className="hidden col-span-2 border lg:flex">Sidebar</div>;
+  return (
+    <div className="h-fit col-span-2 border border-neutral-800 rounded-3xl p-4 lg:flex lg:flex-col">
+      <p>PVP</p>
+      <div className="flex flex-col mt-1">
+        <Link href="/games/jackpot/">
+          <div className="btn p-4 text-left text-neutral-500 w-full rounded-2xl hover:bg-neutral-900">
+            Jackpot
+          </div>
+        </Link>
+        <Link href="/games/jackpot-dice/">
+          <div className="btn p-4 text-left text-neutral-500 w-full rounded-2xl hover:bg-neutral-900">
+            Jackpot Dice
+          </div>
+        </Link>
+      </div>
+      <p>PVE</p>
+      <div className="flex flex-col mt-1">
+        <Link href="/games/crash/">
+          <div className="btn p-4 text-left text-neutral-500 w-full rounded-2xl hover:bg-neutral-900">
+            Crash
+          </div>
+        </Link>
+        <Link href="/games/mines/">
+          <div className="btn p-4 text-left text-neutral-500 w-full rounded-2xl hover:bg-neutral-900">
+            Mines
+          </div>
+        </Link>
+        <Link href="/games/dice/">
+          <div className="btn p-4 text-left text-neutral-500 w-full rounded-2xl hover:bg-neutral-900">
+            Dice
+          </div>
+        </Link>
+      </div>
+      <p>Прокачка уровня</p>
+      <div className="flex flex-col mt-1">
+        <Link href="/games/clicker-xp/">
+          <div className="btn p-4 text-left text-neutral-500 w-full rounded-2xl hover:bg-neutral-900">
+            Кликер XP
+          </div>
+        </Link>
+      </div>
+      <div className="flex justify-between">
+        <Button variant="ghost">
+          <FlagIcon />
+        </Button>
+        <Button variant="ghost">
+          <VolumeIcon />
+        </Button>
+        <Button variant="ghost">
+          <SettingsIcon />
+        </Button>
+        <Button variant="ghost">
+          <BellIcon />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
