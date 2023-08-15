30c943249656ec12d81dfb8444f2e2848f306a62
Dmitrii Malakhov <mitia2022@gmail.com>
2023-08-15 16:59:58 +0200

    Update src/lib/ws.ts

--- src/lib/ws.ts
+++ src/lib/ws.ts
@@ -2,7 +2,7 @@ import config from '@/lib/config';
 import Centrifuge from 'centrifuge';
 import { useEffect } from 'react';
 
-const isSecureProtocol = document.location.href.search('https') === 0;
+const isSecureProtocol = document.location.protocol.includes("https");
 
 const centrifuge = new Centrifuge(isSecureProtocol ? 'wss' : 'ws', {
   refreshEndpoint: `${config.baseUrl}/ajax/wss/refresh`,
