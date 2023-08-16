dfdc39fc64f7982117adf44eb376912af7737f6a
Maksim Peshekhonov <kassady71@gmail.com>
2023-08-16 21:04:58 +0300

    api config fix

--- src/lib/ws.ts
+++ src/lib/ws.ts
@@ -2,20 +2,12 @@ import config from '@/lib/config';
 import Centrifuge from 'centrifuge';
 import { useEffect } from 'react';
 
-const isBrowser = () =>
-  typeof window !== 'undefined' && typeof window.document !== 'undefined';
-const isSecureProtocol =
-  isBrowser() && document.location.protocol.includes('https');
-
-const centrifuge = new Centrifuge(
-  `${isSecureProtocol ? 'wss' : 'ws'}://${config.wsUrl}/websocket`,
-  {
-    refreshEndpoint: `${config.baseUrl}/ajax/wss/refresh`,
-    subscribeEndpoint: `${config.baseUrl}/ajax/wss/subscribe`,
-    subscribeHeaders: { 'X-CSRF-TOKEN': 'qwerty' },
-    refreshHeaders: { 'X-CSRF-TOKEN': 'qwerty' },
-  },
-);
+const centrifuge = new Centrifuge(`${config.wsUrl}/websocket`, {
+  refreshEndpoint: `${config.baseUrl}/ajax/wss/refresh`,
+  subscribeEndpoint: `${config.baseUrl}/ajax/wss/subscribe`,
+  subscribeHeaders: { 'X-CSRF-TOKEN': 'qwerty' },
+  refreshHeaders: { 'X-CSRF-TOKEN': 'qwerty' },
+});
 
 const useCentrifuge = () => {
   useEffect(() => {
